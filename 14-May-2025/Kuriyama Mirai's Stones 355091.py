0617070e8af20c41e955d822700036799523fd8b
Natnael Desalegn <natnaeljul7@gmail.com>
2025-05-14 11:38:36 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 14-May-2025/Kuriyama Mirai's Stones 355091.py
@@ -0,0 +1,24 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+import sys
+n = int(sys.stdin.readline())
+cost = list(map(int, sys.stdin.readline().split()))
+prefix_cost = cost.copy()
+for i in range(1, n): prefix_cost[i]+=prefix_cost[i-1]
+
+cost_sorted = sorted(cost)
+prefix_cost_sorted = cost_sorted
+for i in range(1, n): prefix_cost_sorted[i]+=prefix_cost_sorted[i-1]
+m = int(sys.stdin.readline())
+for _ in range(m):
+    q, l, r = map(int, sys.stdin.readline().split())
+    if q == 1:
+        if l == 1:
+            print(prefix_cost[r-1])
+        else:
+            print(prefix_cost[r-1] - prefix_cost[l-2])
+    else:
+        if l == 1:
+            print(prefix_cost_sorted[r-1])
+        else:
+            print(prefix_cost_sorted[r-1] - prefix_cost_sorted[l-2])
