ac3f83b58b215faabe313c64cfbf995e146e49f0
Natnael Desalegn <natnaeljul7@gmail.com>
2025-05-16 21:43:35 +0300

    Created 'Array Elimination' by A2SV Hub

---
+++ 16-May-2025/Array Elimination 357233.py
@@ -0,0 +1,24 @@
+# Problem: Array Elimination - https://codeforces.com/contest/1601/problem/A
+
+from collections import defaultdict
+import sys
+tests = int(sys.stdin.readline())
+for _ in range(tests):
+    n = int(sys.stdin.readline())
+    nums = list(map(int, sys.stdin.readline().split()))
+    state = [0]*32
+    for num in nums:
+        for i in range(32):
+            if num & (1 << i):
+                state[i] += 1
+
+    min_cnt = n
+
+    for k in state:
+        if k != 0 and k < min_cnt:
+            min_cnt = k
+    res = []
+    for i in range(1, min_cnt + 1):
+        if all(nm % i == 0 for nm in state if nm != 0):
+            res.append(i)
+    print(*res)
