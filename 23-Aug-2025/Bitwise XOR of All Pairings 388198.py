8ea84b1512ddffb80c042f7be2a8a74a424f00c6
Natnael Desalegn <natnaeljul7@gmail.com>
2025-08-23 23:54:05 +0300

    Created 'Bitwise XOR of All Pairings' by A2SV Hub

---
+++ 23-Aug-2025/Bitwise XOR of All Pairings 388198.py
@@ -0,0 +1,12 @@
+# Problem: Bitwise XOR of All Pairings - https://leetcode.com/problems/bitwise-xor-of-all-pairings/description/?envType=problem-list-v2&envId=brainteaser
+
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = 0
+        if len(nums2) & 1:      
+            for v in nums1:
+                ans ^= v
+        if len(nums1) & 1:             
+            for v in nums2:
+                ans ^= v
+        return ans
